2df58927ebed78c95a4baa3c9504668421a3d3ba
Juanan Fernandez <juanan.fernandez@gmail.com>
2022-05-12 19:11:59 +0200

    Added bump effect HeaderCartButton

--- src/components/Layout/Header/HeaderCartButton.js
+++ src/components/Layout/Header/HeaderCartButton.js
@@ -1,17 +1,29 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartIcon from '../../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
 import CartContext from '../../../store/cart-context';
 
 const HeaderCartButton = ({ children, onClickCartButton }) => {
 	const cartCtx = useContext(CartContext);
+	const [isBumped, setIsBumped] = useState(false);
 
 	const totalItems = cartCtx.items.reduce((acc, curr) => {
 		return acc + curr.units;
 	}, 0);
 
+	useEffect(() => {
+		setIsBumped(true);
+
+		const timer = setTimeout(() => {
+			setIsBumped(false);
+		}, 300);
+		return () => clearTimeout(timer);
+	}, [totalItems]);
+
+	const stylesButton = `${styles.button} ${isBumped ? styles.bump : ''}`;
+
 	return (
-		<button className={styles.button} onClick={onClickCartButton}>
+		<button className={stylesButton} onClick={onClickCartButton}>
 			<span className={styles.icon}>
 				<CartIcon />
 			</span>
